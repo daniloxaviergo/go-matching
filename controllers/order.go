66d0969f8f7653416063e0cd42480cf436c6f952
brunoneville31 <brunoneville31@gmail.com>
2023-09-15 12:26:30 -0300

    Create router and controller

---
+++ controllers/order.go
@@ -0,0 +1,73 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type OrderBookResponse struct {
+	Market string    `json:"market"`
+	Asks   []float64 `json:"asks"`
+	Bids   []float64 `json:"bids"`
+}
+
+type OrderRemoveResponse struct {
+	Id     string `json:"id"`
+	Market string `json:"market"`
+	Status string `json:"status"`
+}
+
+type OrderCreateResponse struct {
+	Id     string `json:"id"`
+	Price  string `json:"price"`
+	Volume string `json:"volume"`
+	Market string `json:"market"`
+	Status string `json:"status"`
+}
+
+func AddOrder(c *gin.Context) {
+	market := c.Param("market")
+	id := c.Query("id")
+	price := c.Query("price")
+	volume := c.Query("volume")
+	
+	if id == "" || price == "" || volume == "" || market == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Faltam parametros"})
+		return
+	}
+
+	var response OrderCreateResponse
+	response.Id = id
+	response.Price = price
+	response.Volume = volume
+	response.Status = "ok"
+	response.Market = market
+
+	c.JSON(http.StatusOK, response)
+}
+
+func RemoveOrder(c *gin.Context) {
+	market := c.Param("market")
+	id := c.Query("id")
+
+	if id == "" || market == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Faltam parametros"})
+		return
+	}
+
+	var response OrderRemoveResponse
+	response.Id = id
+	response.Status = "cancel"
+	response.Market = market
+
+	c.JSON(http.StatusOK, response)
+}
+
+func OrderBook(c *gin.Context) {
+	market := c.Param("market")
+
+	var response OrderBookResponse
+	response.Market = market
+
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	"fmt"
 
-	"go-matching/test"
+	"github.com/gin-gonic/gin"
+	"go-matching/controllers"
 )
 
-func main() {
-	fmt.Println("Hello World!!")
-	c := test.Creature{
-		Name: "Sammy the Shark",
-	}
+// func main() {
+// 	fmt.Println("Hello World!!")
+// 	c := test.Creature{
+// 		Name: "Sammy the Shark",
+// 	}
 
-	fmt.Println(c.Name)
+// 	fmt.Println(c.Name)
+// }
+
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/:market/order-create", controllers.AddOrder)
+	router.GET("/:market/order-cancel", controllers.RemoveOrder)
+	router.GET("/:market/orderbook", controllers.OrderBook)
+
+	router.Run("localhost:8080")
 }

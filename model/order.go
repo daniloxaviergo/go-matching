008ceb2ad4fe99821660b1c85d1ccce8b49d7d54
Gerson L Barizon Martins <gerson.martins@foxbit.com.br>
2023-09-15 11:27:13 -0300

    Cola

--- model/order.go
+++ model/order.go
@@ -20,3 +20,19 @@ type Market struct {
 type OrderBook struct {
 	Levels []OrderLevel
 }
+
+/*
+func (os *OrderLevel) Add(order Order) (value int, err error) {
+	// logica
+	return 3, nil
+}
+
+func call() {
+	ol := OrOrderLevel
+	v, err := ol.Add(order)
+	if (err == nil) {
+		// catch exception
+	}
+
+}
+*/
